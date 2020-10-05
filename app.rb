a10be8ba377bed8725a0033a2121e2d8197baa6f
zcammack <zack.cammack@gmail.com>
2020-10-04 20:41:42 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "thisisasecret"
+    end
+
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session["item"] = params[:item]
+        @session = session
+    end
+
 end
